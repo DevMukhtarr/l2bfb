b45fe05251a79c3ad0f045b4bdc81c3815404da9
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-08-31 12:36:43 +0100

    load env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken")
-
-const config = process.env;
+require("dotenv").config()
 
 const verifyToken = (req, res, next) => {
   const token =
@@ -9,7 +8,7 @@ const verifyToken = (req, res, next) => {
     return res.status(403).send("A token is required for authentication");
   }
   try {
-    const decoded = jwt.verify(token, config.JWT_TOKEN_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
     return res.status(401).json({
